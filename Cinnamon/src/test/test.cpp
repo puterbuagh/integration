4f452823180c5aa706bfc437dca6314163336bd8
puterbuagh <126985461+puterbuagh@users.noreply.github.com>
2023-03-09 14:32:08 +0800

    Add files via upload

---
+++ Cinnamon/src/test/test.cpp
@@ -0,0 +1,36 @@
+/*
+    Cinnamon UCI chess engine
+    Copyright (C) Giuseppe Cannella
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#if defined(FULL_TEST)
+
+#include "see.cpp"
+#include "pin.cpp"
+#include "eval.cpp"
+#include "search.cpp"
+#include "perft.cpp"
+#include "syzygy.cpp"
+#include "gtb.cpp"
+#include "fileUtil.cpp"
+#include "string.cpp"
+#include "stalemate.cpp"
+
+#include "spinlockShared.cpp"
+#include "spinlock.cpp"
+#include "perft960.cpp"
+
+#endif
